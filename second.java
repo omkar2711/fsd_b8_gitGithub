2139b2031244243ce6a017aafe825190d7fc5151
omkar bharitkar <omkar.tsx@gmail.com>
2025-07-30 18:22:58 +0530

    second commit

--- second.java
+++ second.java
@@ -1,5 +1,6 @@
 public class second {
     public static void main(String[] args) {
         System.out.println("Hello from the second file!");
+        System.out.println("Execution complete in second file.");
     }
 }
