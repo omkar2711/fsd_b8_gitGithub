f01895a82809a55260fa262c3428096a47d95ee8
omkar bharitkar <omkar.tsx@gmail.com>
2025-08-01 17:33:25 +0530

    first commit on newBranch

--- second.java
+++ second.java
@@ -2,5 +2,6 @@ public class second {
     public static void main(String[] args) {
         System.out.println("Hello from the second file!");
         System.out.println("Execution complete in second file.");
+        System.out.println("Additional output from the second file.");
     }
 }
